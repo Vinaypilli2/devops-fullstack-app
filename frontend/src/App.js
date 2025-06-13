2d739b59fef613945760ba924dfdfa131e8150f8
Vinaypilli2 <vinaypilli56@gmail.com>
2025-06-13 13:51:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 // Base server URL from environment variable + endpoint
-const SERVER_URL = `${process.env.REACT_APP_SERVER_URL}/employees`;
+const SERVER_URL = `${process.env.REACT_APP_SERVER_URL}`;
 
 function App() {
     const [employees, setEmployees] = useState([]);
